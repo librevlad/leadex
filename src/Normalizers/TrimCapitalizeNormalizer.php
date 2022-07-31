2710dc8c5b1db64c7702c2f60da65bfc7500d531
librevlad <librevlad@gmail.com>
2022-07-31 19:09:49 +0300

    wip

--- src/Normalizers/TrimCapitalizeNormalizer.php
+++ src/Normalizers/TrimCapitalizeNormalizer.php
@@ -8,11 +8,11 @@ use Validator;
 
 class TrimCapitalizeNormalizer extends Normalizer {
 
-    protected function normalize( $email ) {
-        return trim( capitalize( $email ) );
-    }
+	protected function normalize( $email ) {
+		return trim( mb_convert_case( $email, MB_CASE_TITLE_SIMPLE ) );
+	}
 
-    protected function validate( $v ) {
-        return Validator::make( compact( 'v' ), [ 'v' => 'required|string|min:2' ] )->passes();
-    }
+	protected function validate( $v ) {
+		return Validator::make( compact( 'v' ), [ 'v' => 'required|string|min:2' ] )->passes();
+	}
 }
