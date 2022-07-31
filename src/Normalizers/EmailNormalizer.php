9cd78bb9fd1f665cbd6a9063eb1a872df0c6cd5c
librevlad <librevlad@gmail.com>
2022-07-31 19:08:34 +0300

    wip

--- src/Normalizers/EmailNormalizer.php
+++ src/Normalizers/EmailNormalizer.php
@@ -8,47 +8,46 @@ use Validator;
 
 class EmailNormalizer extends Normalizer {
 
-    protected function normalize( $email ) {
-        $eml = trim( lowercase( $email ) );
+	protected function normalize( $email ) {
+		$eml = trim( mb_convert_case( $email, MB_CASE_LOWER ) );
+		try {
+			$parts = @explode( '@', $email );
+			if ( strlen( $parts[ 0 ] ) < 6 ) {
+				if ( in_array( $parts[ 1 ], [
+					'mail.ru',
+					'bk.ru',
+					'gmail.com',
+					'rambler.ru',
+					'yandex.ru',
+					'ya.ru',
+					'test.ru',
+				] ) ) {
+					return null;
+				}
 
-        try {
-            $parts = @explode( '@', $email );
-            if ( strlen( $parts[ 0 ] ) < 6 ) {
-                if ( in_array( $parts[ 1 ], [
-                    'mail.ru',
-                    'bk.ru',
-                    'gmail.com',
-                    'rambler.ru',
-                    'yandex.ru',
-                    'ya.ru',
-                    'test.ru',
-                ] ) ) {
-                    return null;
-                }
+				try {
+					$host = explode( '.', $parts[ 1 ] );
 
-                try {
-                    $host = explode( '.', $parts[ 1 ] );
+					if ( strlen( $host ) < 5 ) {
+						return null;
+					}
 
-                    if ( strlen( $host ) < 5 ) {
-                        return null;
-                    }
+				}
+				catch ( \Exception $e ) {
+					return null;
+				}
 
-                }
-                catch ( \Exception $e ) {
-                    return null;
-                }
+			}
 
-            }
+		}
+		catch ( \Exception $e ) {
+			return null;
+		}
 
-        }
-        catch ( \Exception $e ) {
-            return null;
-        }
+		return $eml;
+	}
 
-        return $eml;
-    }
-
-    protected function validate( $email ) {
-        return Validator::make( compact( 'email' ), [ 'email' => 'required|email' ] )->passes();
-    }
+	protected function validate( $email ) {
+		return Validator::make( compact( 'email' ), [ 'email' => 'required|email' ] )->passes();
+	}
 }
--- src/Normalizers/NoRepeatNormalizer.php
+++ src/Normalizers/NoRepeatNormalizer.php
@@ -8,24 +8,24 @@ use Validator;
 
 class NoRepeatNormalizer extends Normalizer {
 
-    protected function normalize( $v ) {
-        $letterstats = [];
+	protected function normalize( $v ) {
+		$letterstats = [];
 
-        for ( $i = 0; $i < strlen( $v ); $i ++ ) {
-            $digit = lowercase( mb_substr( $v, $i, 1 ) );
+		for ( $i = 0; $i < strlen( $v ); $i ++ ) {
+			$digit = trim( mb_convert_case( mb_substr( $v, $i, 1 ), MB_CASE_LOWER ) );
 
-            $letterstats[ $digit ] = isset( $letterstats[ $digit ] ) ? $letterstats[ $digit ] + 1 : 1;
-        }
+			$letterstats[ $digit ] = isset( $letterstats[ $digit ] ) ? $letterstats[ $digit ] + 1 : 1;
+		}
 
-        if ( count( $letterstats ) < 2 ) {
-            return null;
-        }
+		if ( count( $letterstats ) < 2 ) {
+			return null;
+		}
 
 
-        return $v;
-    }
+		return $v;
+	}
 
-    protected function validate( $v ) {
-        return Validator::make( compact( 'v' ), [ 'v' => 'required|string|min:2' ] )->passes();
-    }
+	protected function validate( $v ) {
+		return Validator::make( compact( 'v' ), [ 'v' => 'required|string|min:2' ] )->passes();
+	}
 }
