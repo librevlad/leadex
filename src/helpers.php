3b3aec8069de35101280ed238e7aff1cc3035524
librevlad <librevlad@gmail.com>
2023-07-16 22:29:49 +0300

    velidator

--- src/helpers.php
+++ src/helpers.php
@@ -1,13 +1,15 @@
 <?php
 
+if (! function_exists('validator')) {
 
-function validator( $dataToValidate, $rules, $messages = [] ) {
-	$filesystem = new Illuminate\Filesystem\Filesystem();
-	$fileLoader = new \Illuminate\Translation\FileLoader( $filesystem, '' );
-	$translator = new Illuminate\Translation\Translator( $fileLoader, 'en_US' );
-	$factory    = new \Illuminate\Validation\Factory( $translator );
+	function validator( $dataToValidate, $rules, $messages = [] ) {
+		$filesystem = new Illuminate\Filesystem\Filesystem();
+		$fileLoader = new \Illuminate\Translation\FileLoader( $filesystem, '' );
+		$translator = new Illuminate\Translation\Translator( $fileLoader, 'en_US' );
+		$factory    = new \Illuminate\Validation\Factory( $translator );
 
-	$validator = $factory->make( $dataToValidate, $rules, $messages );
+		$validator = $factory->make( $dataToValidate, $rules, $messages );
 
-	return $validator;
+		return $validator;
+	}
 }
